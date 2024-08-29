22295d9edeab880367f3ee83fc7b35d3d60e5722
Chinmay-Jadhav-04 <[chinmayrjadhav4122003@gmail.com]>
2024-08-29 15:40:08 +0530

    commit

---
+++ JobManagementSystem/src/main/java/com/job/exception/JobNotFound.java
@@ -0,0 +1,9 @@
+package com.job.exception;
+
+public class JobNotFound extends RuntimeException{
+
+	public JobNotFound(String msg) {
+		super(msg);
+	}
+
+}
---
+++ JobManagementSystem/src/main/java/com/job/repo/JobRepository.java
@@ -0,0 +1,12 @@
+package com.job.repo;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.job.entity.Job;
+
+@Repository
+public interface JobRepository extends JpaRepository<Job,Integer>{
+
+}
